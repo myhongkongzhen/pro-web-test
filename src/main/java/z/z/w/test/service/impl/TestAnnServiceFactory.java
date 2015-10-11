cdad85067d609b97b175d1629e27948f6b383e3b
Z_Z.W <myhongkongzhen@gmail.com>
2015-10-11 23:14:59 +0800

    配置方式 修改spring接口注入方法，採用業務接口注入 private IService testAnnServiceImpl;

---
+++ src/main/java/z/z/w/test/service/impl/TestAnnServiceFactory.java
@@ -0,0 +1,28 @@
+/**********************************************************************************************************************
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.                                       *
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.                        *
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.                                                   *
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.                     *
+ * Vestibulum commodo. Ut rhoncus gravida arcu.                                                                       *
+ **********************************************************************************************************************/
+
+package z.z.w.test.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import z.z.w.test.service.IBussOper;
+import z.z.w.test.service.IService;
+
+/*********************************************************************************************
+ * <pre>
+ *     FileName: z.z.w.test.service.impl.TestAnnServiceImpl
+ *         Desc:
+ *       author: Z_Z.W - myhongkongzhen@gmail.com
+ *      version: 2015-10-10 17:55
+ *   LastChange: 2015-10-10 17:55
+ *      History:
+ * </pre>
+ *********************************************************************************************/
+public abstract class TestAnnServiceFactory implements IBussOper, IService {
+    Logger logger = LoggerFactory.getLogger(TestAnnServiceFactory.class);
+}
